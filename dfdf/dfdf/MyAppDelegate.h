3012816ef8265a3ed9cedd9715fd5833097d410f
Cory Sullivan <cory.sullivan@me.com>
2012-10-08 21:16:54 -0300

    commot

---
+++ dfdf/dfdf/MyAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MyAppDelegate.h
+//  dfdf
+//
+//  Created by Cory Sullivan on 2012-10-08.
+//  Copyright (c) 2012 Cory Sullivan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ dfdf/dfdf/MyAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  MyAppDelegate.m
+//  dfdf
+//
+//  Created by Cory Sullivan on 2012-10-08.
+//  Copyright (c) 2012 Cory Sullivan. All rights reserved.
+//
+
+#import "MyAppDelegate.h"
+
+@implementation MyAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
